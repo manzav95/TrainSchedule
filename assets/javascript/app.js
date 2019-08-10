edefab33621e96dce846f802591fb23f944599a3
manzav95 <zavalamanuel95@gmail.com>
2019-08-10 13:54:52 -0700

    Setup main index and assets folder. linked firebase to JS

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+var config = {
+    apiKey: "AIzaSyAjKb2BMs1vJjvqeGFzAlM8Mo-CJIBp1hU",
+    authDomain: "trainmane.firebaseapp.com",
+    databaseURL: "https://trainmane.firebaseio.com",
+    projectId: "trainmane",
+    storageBucket: "",
+    messagingSenderId: "908932599272",
+    appId: "1:908932599272:web:c3e825c57388e7c2"
+  };
+
+  let database = initializeApp(config);
+
+  let name = "";
+  let destination = "";
+  let time = "";
+  
