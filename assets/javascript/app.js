325e6bd2d2af01b7085afaaaf4743f718a0c5320
manzav95 <zavalamanuel95@gmail.com>
2019-08-11 23:35:45 -0700

    created a bootstrap webpage to display train times. Linked firebase and added functionality using JS.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,14 +3,52 @@ var config = {
     authDomain: "trainmane.firebaseapp.com",
     databaseURL: "https://trainmane.firebaseio.com",
     projectId: "trainmane",
-    storageBucket: "",
+    storageBucket: "https://trainmane.firebaseio.com",
     messagingSenderId: "908932599272",
     appId: "1:908932599272:web:c3e825c57388e7c2"
   };
 
-  let database = initializeApp(config);
+  firebase.initializeApp(config);
+
+  let database = firebase.database();
 
   let name = "";
   let destination = "";
   let time = "";
+  let reccurence = "";
+
+  $("#submit-train").on("click", function(){
+      event.preventDefault();
+      name = $("#train-name").val().trim();
+      destination = $("#destination").val().trim();
+      time = moment($("#first-train").val().trim(), "HH:mm").subtract(1, "years").format("X");
+      reccurence = $("#frequency").val().trim();
+
+      database.ref().push({
+          name,
+          destination,
+          time,
+          reccurence: `${reccurence} mins`
+      });
+
+      $("#train-name").val("");
+      $("#destination").val("");
+      $("#first-train").val("");
+      $("#frequency").val("");
   
+    });
+
+    database.ref().on("child_added", function(snapshot){
+        $("#train-name").text(snapshot.val().name)
+        $("#destination").text(snapshot.val().destination)
+        $("#first-train").text(snapshot.val().time)
+        $("#frequency").text(snapshot.val().reccurence)
+        
+        let remainder = moment().diff(moment(time,"X"),"minutes") % reccurence;
+        let minutes = reccurence - remainder;
+        let arrival = moment().add(minutes,"minutes").format("hh:mm A")
+        let newTr = $("<tr>")
+        newTr.html(`<td>${name}</td><td>${destination}</td><td>${reccurence}</td><td>${arrival}</td><td>${minutes}</td>`)
+        $("#main-holder").append(newTr)
+    
+    })
